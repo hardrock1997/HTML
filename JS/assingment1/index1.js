12015994439eea9efedde6a9109179c9699c1387
vicfirth@1997 <yashmundra1997@gmail.com>
2025-02-10 20:39:39 +0530

    js assingment started

---
+++ JS/assingment1/index1.js
@@ -0,0 +1,25 @@
+const p=prompt("Enter the number of users");
+let pInt=Number(p);
+const names=[];
+while(pInt>0) {
+    const name=prompt("Enter the name of the user")
+    pInt=pInt-1;
+    names.push(name);
+}
+const div=document.getElementById("names");
+const ol=document.createElement("ol");
+
+function insertUserNamesInDiv(names) {
+    const n=names.length;
+    for(let i=0;i<n;++i) {
+        createListElement(names[i]);
+    }
+}
+
+function createListElement(name) {
+    const li=document.createElement("li");
+    li.textContent=name;
+    ol.appendChild(li);
+}
+insertUserNamesInDiv(names);
+div.appendChild(ol);
