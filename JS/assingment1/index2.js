fe0a4abaae6af7825cb6ccfd3dc0c0ad4a453e9f
vicfirth@1997 <yashmundra1997@gmail.com>
2025-02-11 21:07:54 +0530

    added part b

--- JS/assingment1/index2.js
+++ JS/assingment1/index2.js
@@ -8,21 +8,69 @@ async function main() {
     const data= await getData()
     const recipes=data.recipes;
     const requiredData=getRequiredData(recipes,[]);
-    displayRequiredData(requiredData);
+    const requiredData1=getRequiredData1(recipes,[]);
+    displayRequiredData(requiredData,requiredData1);
 }
 main();
 
 
-function displayRequiredData(data) {
+function displayRequiredData(data1=[],data2=[]) {
     const div=document.getElementById("name_cuisines");
+    const h1=document.createElement('h1');
+    h1.textContent="Total Recipes";
     const ul=document.createElement("ul");
-    for(let i=0;i<data.length;++i) {
-        const li=document.createElement("li");
-        li.textContent=`${i+1} : Name:${data[i][i+1]['name']},Cuisine:${data[i][i+1]['cuisine']}`;
-        ul.appendChild(li);
+    const div2=document.getElementById("filtered_name_cuisines");
+    const h2=document.createElement('h1');
+    h2.textContent="Filtered Recipes by rating > 4.5";
+    const ul2=document.createElement("ul");
+    if(data1) {
+        for(let i=0;i<data1.length;++i) {
+            const li=document.createElement("li");
+            li.textContent=`${i+1} : Name:${data1[i][i+1]['name']},Cuisine:${data1[i][i+1]['cuisine']}`;
+            ul.appendChild(li);
+        }
+        div.appendChild(h1);
+        div.appendChild(ul);
     }
-    div.appendChild(ul);
+    if(data2) {
+        for(let i=0;i<data2.length;++i) {
+            const li=document.createElement("li");
+            console.log(data2[i]['name'],data2[i]['id'])
+            li.textContent=`${i+1} : Name:${data2[i]['name']},Cuisine:${data2[i]['cuisine']}`;
+            ul2.appendChild(li);
+        }
+        div2.appendChild(h2);
+        div2.appendChild(ul2);
+    }
+ 
+}
 
+
+function getRequiredData1(recipes,arr) {
+    //--------for loop---------
+    for(let i=0;i<recipes.length;++i) {
+        if(recipes[i]['rating']>4.5) {
+            let obj={}
+            obj=recipes[i];
+            arr.push(obj);
+        }
+    }
+
+
+    //--------for Each---------
+    // recipes.forEach((rec)=>{
+    //     let obj={}
+    //     obj=rec
+    //     arr.push(obj);
+    // })
+
+    //--------map---------
+    // recipes.map((rec)=>{
+    //     let obj={}
+    //     obj=rec
+    //     arr.push(obj);
+    // })
+    return arr;
 }
 
 function getRequiredData(recipes,arr) {
@@ -36,25 +84,25 @@ function getRequiredData(recipes,arr) {
         arr.push(obj);
     }
 
-    //--------for Each---------
-    recipes.forEach((rec)=>{
-        const obj={};
-        obj[rec.id]={
-            "name":rec.name,
-            "cuisine":rec.cuisine
-        }
-        arr.push(obj);
-    })
+    // //--------for Each---------
+    // recipes.forEach((rec)=>{
+    //     const obj={};
+    //     obj[rec.id]={
+    //         "name":rec.name,
+    //         "cuisine":rec.cuisine
+    //     }
+    //     arr.push(obj);
+    // })
 
-    //--------map---------
-    recipes.map((rec)=>{
-        const obj={};
-        obj[rec.id]={
-            "name":rec.name,
-            "cuisine":rec.cuisine
-        }
-        arr.push(obj);
-    })
+    // //--------map---------
+    // recipes.map((rec)=>{
+    //     const obj={};
+    //     obj[rec.id]={
+    //         "name":rec.name,
+    //         "cuisine":rec.cuisine
+    //     }
+    //     arr.push(obj);
+    // })
     
     return arr;
 }
