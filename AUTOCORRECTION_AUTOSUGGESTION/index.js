101a1d1a7f5a7b8358a26bdfeccd4919da5214ad
vicfirth@1997 <yashmundra1997@gmail.com>
2025-02-22 17:36:46 +0530

    auto_project_v1

--- AUTOCORRECTION_AUTOSUGGESTION/index.js
+++ AUTOCORRECTION_AUTOSUGGESTION/index.js
@@ -1,41 +1,60 @@
-import { autoCompleteUrl, autoCompleteOptions, autoSuggestionsUrlOptions, autoSuggestionsUrl } from "./utils.js";
+import {
+  autoCompleteUrl,
+  autoCompleteOptions,
+  autoSuggestionsUrlOptions,
+  autoSuggestionsUrl,
+} from "./utils.js";
 
-function suggestionsHelper(suggestions,suggestionsContainer) {
-    suggestionsContainer.innerHTML = "";
-    const heading = document.createElement("h2");
-    heading.innerText = "Suggestions";
-    heading.style.color = "white";
-    heading.style.textAlign="center";
-    suggestionsContainer.appendChild(heading);
-    const suggs = suggestions?.filter((_, i) => i < 3);
-    suggs?.forEach((sugg) => {
-      const sug = document.createElement("h4");
-      sug.style.color = "white";
-      sug.style.textAlign="center";
-      sug.innerText = sugg.query;
-      suggestionsContainer.appendChild(sug);
-    });
+function suggestionsHelper(suggestions, suggestionsContainer) {
+  suggestionsContainer.innerHTML = "";
+  const heading = document.createElement("h2");
+  heading.innerText = "Suggestions";
+  heading.style.color = "white";
+  heading.style.textAlign = "center";
+  suggestionsContainer.appendChild(heading);
+  const suggs = suggestions?.filter((_, i) => i < 3);
+  suggs?.forEach((sugg) => {
+    const sug = document.createElement("h4");
+    sug.style.color = "white";
+    sug.style.textAlign = "center";
+    sug.innerText = sugg.query;
+    suggestionsContainer.appendChild(sug);
+  });
 }
 
-function completionsHelper(completions,suggestionsContainer) {
-    const heading2=document.createElement("h2");
-    heading2.textContent="Completions";
-    heading2.style.color = "white";
-    heading2.style.textAlign="center";
-    suggestionsContainer.appendChild(heading2);
-    completions.forEach((com)=>{
-      const cmp = document.createElement("h4");
-      cmp.style.color = "white";
-      cmp.style.textAlign="center";
-      cmp.innerText=com.text;
-      suggestionsContainer.appendChild(cmp);
-    })
+function completionsHelper(completions, suggestionsContainer) {
+  const heading2 = document.createElement("h2");
+  heading2.textContent = "Completions";
+  heading2.style.color = "white";
+  heading2.style.textAlign = "center";
+  suggestionsContainer.appendChild(heading2);
+  completions.forEach((com) => {
+    const cmp = document.createElement("h4");
+    cmp.style.color = "white";
+    cmp.style.textAlign = "center";
+    cmp.innerText = com.text;
+    suggestionsContainer.appendChild(cmp);
+  });
 }
 
+function generateSuggestions(suggestions, suggestionsContainer, completions) {
+  suggestionsHelper(suggestions, suggestionsContainer);
+  completionsHelper(completions, suggestionsContainer);
+}
 
-function generateSuggestions(suggestions, suggestionsContainer,completions) {
-  suggestionsHelper(suggestions,suggestionsContainer)
-  completionsHelper(completions,suggestionsContainer)
+function handleLoadingBar(delta) {
+  const loadingBar = document.getElementById("loading_bar");
+  loadingBar.style.display = "block";
+  let timer = null;
+  let co = 0;
+  while (co < delta && co < 35) {
+    loadingBar.style.width = `${co}%`;
+    co++;
+  }
+  if (timer) clearTimeout(timer);
+  timer = setTimeout(() => {
+    loadingBar.style.display = "none";
+  }, 1000);
 }
 
 async function getAutoCompleteSuggestions(inputValue) {
@@ -46,16 +65,26 @@ async function getAutoCompleteSuggestions(inputValue) {
     //clearing the UI if nothing is searched.
     suggestionsContainer.style.display = "none";
   } else {
+    let x1 = Date.now();
     suggestionsContainer.style.display = "flex";
-    autoCompleteOptions.body=JSON.stringify({text:inputValue,language: 'en'});
-    const [suggestionsData,completionsData]= await Promise.all([fetch(
-          autoSuggestionsUrl + inputValue,
-          autoSuggestionsUrlOptions
-        ),fetch(autoCompleteUrl,autoCompleteOptions)]);
-    const suggestions= await suggestionsData.json();
+    autoCompleteOptions.body = JSON.stringify({
+      text: inputValue,
+      language: "en",
+    });
+    const [suggestionsData, completionsData] = await Promise.all([
+      fetch(autoSuggestionsUrl + inputValue, autoSuggestionsUrlOptions),
+      fetch(autoCompleteUrl, autoCompleteOptions),
+    ]);
+    const suggestions = await suggestionsData.json();
     const completions = await completionsData.text();
+    let x2 = Date.now();
+    handleLoadingBar(x2 - x1);
     suggestionsContainer.style.backgroundColor = "black";
-    generateSuggestions(suggestions?.data, suggestionsContainer,JSON.parse(completions).predictions);
+    generateSuggestions(
+      suggestions?.data,
+      suggestionsContainer,
+      JSON.parse(completions).predictions
+    );
   }
 }
 
@@ -63,7 +92,9 @@ function debounce(fn, delay) {
   let timer;
   return function (...args) {
     clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
   };
 }
 
